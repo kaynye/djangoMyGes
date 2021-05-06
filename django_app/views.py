e0a25c4c54c7948ad0239affd0bc2588186bf287
Sami75 <sami.thairi@hotmail.fr>
2021-05-06 11:55:06 +0200

    Improvement in prof pages

--- django_app/views.py
+++ django_app/views.py
@@ -1,45 +1,65 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_protect,csrf_exempt
-from django.contrib.auth import login, authenticate,logout
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from django.contrib.auth import login, authenticate, logout
 from django_app.models import *
-from django.contrib import messages #import messages
+from django.contrib import messages  # import messages
+import django.utils.timezone as timezone
 
 
 @login_required
 def home(request):
-    return render(request,"django_app/home.html")
+    return render(request, "django_app/home.html")
 
 
 def login_page(request):
     print(request.user)
     if request.user.is_authenticated:
-        return redirect('django_app:home')
+        return redirect("django_app:home")
     else:
-        return render(request,"django_app/index.html")
+        return render(request, "django_app/index.html")
+
 
 @login_required
 def profile(request):
-    getClass = Classroom.objects.filter(c_student=request.user).all().order_by("c_promotion").first()
+    getClass = (
+        Classroom.objects.filter(c_student=request.user)
+        .all()
+        .order_by("c_promotion")
+        .first()
+    )
 
     print(getClass)
-    return render(request,"django_app/profile.html", {"class": getClass})
+    return render(request, "django_app/profile.html", {"class": getClass})
+
 
 @login_required
 def planning(request):
     return render(request, "django_app/planning.html")
 
+
 def notes(request):
     return render(request, "django_app/notes.html")
 
+
 @login_required
 def change_password(request):
-    if request.POST.get("actual_password") and request.POST.get("new_password") and request.POST.get("re_password"):
-        if authenticate(username=request.user.username, password=request.POST.get("actual_password")) is not None:
+    if (
+        request.POST.get("actual_password")
+        and request.POST.get("new_password")
+        and request.POST.get("re_password")
+    ):
+        if (
+            authenticate(
+                username=request.user.username,
+                password=request.POST.get("actual_password"),
+            )
+            is not None
+        ):
             if request.POST.get("new_password") == request.POST.get("re_password"):
                 request.user.set_password(request.POST.get("new_password"))
                 request.user.save()
-                messages.success(request, "Le mot de passe à correctement été changée." )
+                messages.success(request, "Le mot de passe à correctement été changée.")
                 return redirect("django_app:login_page")
             else:
                 messages.error(request, "Les mots de passes ne correspondent pas.")
@@ -48,49 +68,67 @@ def change_password(request):
             messages.error(request, "Le mot de passe actuel est incorrect.")
             return redirect("django_app:profile")
 
+
 @login_required
 def student_class(request):
-    getClass = Classroom.objects.filter(c_student=request.user).all().order_by("c_promotion").first()
+    getClass = (
+        Classroom.objects.filter(c_student=request.user)
+        .all()
+        .order_by("c_promotion")
+        .first()
+    )
     getStudents = getClass.c_student.all()
-    return render(request, "django_app/classes.html", {"students": getStudents, "class": getClass})
+    return render(
+        request, "django_app/classes.html", {"students": getStudents, "class": getClass}
+    )
+
 
 @csrf_exempt
 def loginUser(request):
-    res={}
+    res = {}
     print(request.POST)
     if request.POST.get("pseudo") and request.POST.get("password"):
-        user = authenticate(username=request.POST.get("pseudo"), password=request.POST.get("password"))
+        user = authenticate(
+            username=request.POST.get("pseudo"), password=request.POST.get("password")
+        )
         if user is not None:
             login(request, user)
-            return redirect('django_app:home')
+            return redirect("django_app:home")
         else:
-            return render(request,"django_app/index.html",{"erreur": True})
+            return render(request, "django_app/index.html", {"erreur": True})
+
 
 @login_required
 def logoutUser(request):
     logout(request)
-    return redirect('django_app:login_page')
+    return redirect("django_app:login_page")
+
 
 @login_required
 def profCourse(request):
     courses = Matiere.objects.all()
 
-    return render(request,"django_app/prof.course.html",{
-            "courses" : courses
-            })
+    return render(request, "django_app/prof.course.html", {"courses": courses})
+
 
 @login_required
-def profCourseStudents(request,id):
+def profCourseStudents(request, id):
     course = Matiere.objects.get(id=id)
 
-    return render(request,"django_app/prof.course.students.html",{
-            "classrooms" : course.m_classroom.all()
-            })
+    return render(
+        request,
+        "django_app/prof.course.students.html",
+        {"classrooms": course.m_classroom.all()},
+    )
+
 
 @login_required
-def profCourseDetails(request,id):
+def profCourseDetails(request, id):
     course = Matiere.objects.get(id=id)
-
-    return render(request,"django_app/prof.course.students.html",{
-            "course" : course
-            })
+    classrooms = course.m_classroom.filter(c_promotion=timezone.now().year)
+    
+    return render(
+        request,
+        "django_app/prof.course.details.html",
+        {"course": course, "classrooms": classrooms},
+    )
