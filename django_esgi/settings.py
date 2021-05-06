d980a22e4d1011a8706c05ca6bcb09986bd65559
OutLawz <proserratomas@gmail.com>
2021-05-06 17:24:31 +0200

    add requirements

--- django_esgi/settings.py
+++ django_esgi/settings.py
@@ -98,7 +98,12 @@ WSGI_APPLICATION = 'django_esgi.wsgi.application'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Database
